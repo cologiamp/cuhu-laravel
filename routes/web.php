2fc4d96dad2ba7d58ac43fe5e6f6f67bfb50bd35
Ignacio Giampaoli <colo92@gmail.com>
2021-02-15 18:11:52 -0300

    Creating Posts model and table migration

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
 
@@ -14,10 +15,12 @@ use App\Http\Controllers\Auth\LogoutController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
 Route::get('/', function () {
     return view('home');
 })->name('home');
+*/
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::post('/', [LoginController::class, 'signin']);
 
